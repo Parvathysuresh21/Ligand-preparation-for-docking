0815bfaf264e8ee1ef710657e005c4faa2d7fd73
ParGit <94734124+Parvathysuresh21@users.noreply.github.com>
2024-03-03 10:15:47 +0530

    Add files via upload

--- obabel.sh
+++ obabel.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-# Check if Open Babel is installed
+
 if ! command -v obabel &> /dev/null; then
     echo "Open Babel is not installed. Please install it first."
     exit 1
 fi
 
-# Directory containing the .sdf files
-input_dir="\home\parvathy\docking\P"
+input_dir="Path to inputfile"
+output_dir="path to outputfile"
 
-# Directory where you want to save the .pdbqt files
-output_dir="\home\parvathy\docking\protein_pdbqt_files"
 
-# Create the output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-# Iterate through each .sdf file in the input directory
+
 for file in "$input_dir"/*.sdf; do
     filename=$(basename "$file")
     base="${filename%.*}"
     
-    # Convert .sdf to .pdbqt
     obabel -isdf "$file" -opdbqt -O "$output_dir/$base.pdbqt"
     
     echo "Converted $file to $output_dir/$base.pdbqt"
